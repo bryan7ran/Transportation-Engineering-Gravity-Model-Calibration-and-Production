fa1934da178905ac59869737d9e74f0f30642c68
bryan7ran <bhtran@ualberta.ca>
2018-10-19 12:54:26 -0600

    rand

--- sandbox.m
+++ sandbox.m
@@ -1,4 +1,17 @@
 row_increment = 0;
-desired_a_search = [-3,3];
 desired_b_search = [-3,3];
+desired_a_search = [-3,3];
+increment = 0.01;
+
+desired_b_search = desired_b_search(1):increment:desired_b_search(2);
+desired_a_search = desired_a_search(1):increment:desired_a_search(2);
+
+RMSErecord = zeros(length(desired_b_search)+1,length(desired_a_search)+1);
+
+for i = 2:length(desired_b_search)+1
+    for j = 2:length(desired_a_search)+1
+        RMSErecord(i,j) = rand(1);
+    end
+end
+
 
